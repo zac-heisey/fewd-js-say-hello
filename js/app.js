e93f726fe1c43d066f68947aecbbaf958f5dd8f5
Tom Kraak <tom.kraak@gmail.com>
2018-11-05 22:15:26 -0500

    small refactor

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
 (function () {
-  var input = document.getElementById('name')
-
-  input.addEventListener('keypress', eventHandler, false)
+  document.documentElement.addEventListener('keypress', eventHandler);
 
   function eventHandler (event) {
+    const input = event.target.closest('#name');
+    const warning = 'text-danger';
+    let message = 'The text field is empty. Please enter your name.';
     if (event.which === 13) {
       if (input.value.length === 0) {
-        var warning = 'text-danger'
-        var message = 'The text field is empty. Please enter your name.'
-        writeToDocument(event, message, warning)
+        writeToDocument(event, message, warning);
       } else {
-        message = 'Hello ' + input.value + ', nice to meet you!'
-        writeToDocument(event, message)
+        message = `Hello ${input.value} nice to meet you!`;
+        writeToDocument(event, message);
       }
     }
   }
 
   function writeToDocument (event, message, warning) {
-    var column = document.querySelector('.col-md-6')
-    var p = document.createElement('p')
-    p.innerHTML = message
-    p.classList.add(warning)
-    column.appendChild(p)
-    event.preventDefault()
+    const column = document.querySelector('.col-md-6');
+    const p = document.createElement('p');
+    p.innerHTML = message;
+    p.classList.add(warning);
+    column.appendChild(p);
+    event.preventDefault();
   }
-})()
-
+})();
