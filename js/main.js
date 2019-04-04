0f957d8bf06bc0089973e7a7c0c8e1a9ac99d32a
Zac Heisey <zheisey@gmail.com>
2019-04-04 19:02:39 -0400

    listen for keydown events

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Get input field
+var input = document.querySelector('input');
+
+// Listen for keydown event on input field
+input.addEventListener('keydown', function(event) {
+  var userInput = input.value;
+  var greeting = document.querySelector('.col-md-6');
+  var p = document.createElement('p');
+
+});
