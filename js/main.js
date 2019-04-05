e11a1fcbb1032eae3aee4df01d6936620a8119f9
Zac Heisey <zheisey@gmail.com>
2019-04-04 20:27:40 -0400

    finish app and make it awesome

--- js/main.js
+++ js/main.js
@@ -7,4 +7,18 @@ input.addEventListener('keydown', function(event) {
   var greeting = document.querySelector('.col-md-6');
   var p = document.createElement('p');
 
-});
+  // If enter key was pressed
+  if (event.key === 'Enter') {
+    if (userInput.length > 0) {
+      p.innerText = `Hello ${userInput}, nice to meet you!`;
+      greeting.appendChild(p);
+    } else {
+      alert('Please enter your name');
+    }
+    // Prevent default behavior
+    event.preventDefault();
+    // Clear input field
+    input.value = '';
+  }
+
+}, false);
