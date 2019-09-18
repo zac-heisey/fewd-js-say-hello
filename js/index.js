1f715d21db0804d3de66119c3a4fb021f0bada27
Zac Heisey <zheisey@gmail.com>
2019-09-18 08:36:27 -0400

    remove unused js files

--- js/index.js
+++
@@ -1,4 +0,0 @@
-(function(exports) {
-  const { eventHandler } = exports;
-  document.documentElement.addEventListener('keypress', eventHandler);
-})(typeof exports === 'undefined' ? window.app : exports);
