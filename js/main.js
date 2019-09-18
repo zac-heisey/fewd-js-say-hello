218ed42bc7861d5df43e4629997c99a7114a7ab4
Zac Heisey <zheisey@gmail.com>
2019-09-18 08:57:21 -0400

    move variables to more appropriate areas of script and add more comments

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 // Get input field
 var input = document.querySelector('input');
 
+// Get element to append greeting to
+var greeting = document.querySelector('.col-md-6');
+
 // Listen for keydown event on input field
 input.addEventListener('keydown', function(event) {
-  var userInput = input.value;
-  var greeting = document.querySelector('.col-md-6');
-  var p = document.createElement('p');
 
   // If enter key was pressed
   if (event.key === 'Enter') {
+    // Get user input
+    var userInput = input.value;
+    // if user input length is not empty
     if (userInput.length > 0) {
+      // Create <p> element
+      var p = document.createElement('p');
+      // Update inner text of created <p> element
       p.innerText = `Hello ${userInput}, nice to meet you!`;
+      // Append created <p> element with inner text to greeting element
       greeting.appendChild(p);
     } else {
       alert('Please enter your name');
